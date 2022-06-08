f9dda08738e5bcaad7003f0fe6eb059ef0abde3b
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-07 22:10:43 -0300

    Return the name, price and a id of the requested product

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const {randomUUID} = require("crypto");
 
 const app = express();
 
-app.get("/primeira-rota", (request, response) => {
-    return response.json({
-        message: "Acessou a primeira rota",
-    });
+app.use(express.json());
+
+const products = [];
+
+/*
+POST => Inserir
+GET => Buscar
+PUT => Alterar
+DELETE => Remover
+
+Body => Utilizado para enviar dados para a aplicação
+Params => /product/abc -> parâmetros de rota. Não são obrigatórios (/)
+Query => /product?id=123&value=2 -> parâmetros que fazem parte da rota mas não são obrigatórios (?)
+*/
+
+app.post("/products", (request, response) => {
+
+    const {name, price} = request.body;
+    const product = {
+        name,
+        price,
+        id: randomUUID(), 
+    }
+    
+    products.push(product);
+    
+    return response.json(product);
+
 });
 
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
