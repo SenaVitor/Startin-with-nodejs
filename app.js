f224835e2f1b2c8fe82a9ebc3c5ad2f9c71bf0f3
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-15 11:39:05 -0300

    Update and delete a product

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {randomUUID} = require("crypto");
+const { response } = require('express');
 
 const app = express();
 
@@ -41,6 +42,25 @@ app.get("/products/:id", (request, response) => {
     const {id} = request.params;
     const product = products.find(product => product.id === id);
     return response.json(product);
+});
+
+app.put("/products/:id", (request, response) => {
+    const {id} = request.params;
+    const { name, price } = request.body;
+    const productIndex = products.findIndex((product) => product.id === id);
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price,
+    };
+    return response.json({ message: "Produto alterado com sucesso" });
+});
+
+app.delete("/products/:id", (request, response) => {
+    const {id} = request.params;
+    const productIndex = products.findIndex((product) => product.id === id);
+    products.splice(productIndex, 1);
+    return response.json({ message: "Produto removido com sucesso" });
 })
 
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
